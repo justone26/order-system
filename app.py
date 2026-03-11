90efcf92162c018f42cb9f54389c1014552a5e3e
justone26 <justone2022@naver.com>
2026-03-11 11:51:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ if st.session_state.df_raw is not None:
         for hist in st.session_state.history[selected_date]:
             st.dataframe(hist, use_container_width=True)
 
-    st.download_button("📥 최종 데이터 다운로드", data=BytesIO(st.session_state.df_raw.to_excel(index=False).encode('utf-8')), file_name="결과.xlsx")
+    st.download_button("📥 최종 데이터 다운로드", data=BytesIO(buffer = BytesIO()pd.ExcelWriter(buffer, ...)buffer.getvalue(), file_name="결과.xlsx")
+
