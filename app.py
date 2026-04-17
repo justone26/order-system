7a13e2dbc68f3a8854ba45295512f55bcc25f962
justone26 <justone2022@naver.com>
2026-04-17 13:02:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -674,8 +674,8 @@ if "db_history" in st.session_state and not st.session_state.db_history.empty:
         column_config={
             "발주시간": st.column_config.TextColumn("발주시간", width=100),
             "공급처": st.column_config.TextColumn("공급처", width=90),
-            "상품명": st.column_config.TextColumn("상품명", width=400),
-            "옵션": st.column_config.TextColumn("옵션", width=80),
+            "상품명": st.column_config.TextColumn("상품명", width=350),
+            "옵션": st.column_config.TextColumn("옵션", width=120),
             "공급처상품명": st.column_config.TextColumn("공급처상품명", width=200),
             "최종잔량": st.column_config.NumberColumn("최종잔량", width=60),
             "비고(처리내역)": st.column_config.TextColumn("비고(처리내역)", width=400), # 비고 확장
